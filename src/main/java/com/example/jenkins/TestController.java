0d55655b067477b37c64fa6922e4716d25edfda9
phuctong2002 <vanphuc15092002@gmail.com>
2023-12-03 00:16:10 +0700

    test

--- src/main/java/com/example/jenkins/TestController.java
+++ src/main/java/com/example/jenkins/TestController.java
@@ -5,7 +5,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TestController {
-//    test trigger
     @GetMapping("/")
     public String viewHomePage() {
         return "index";
